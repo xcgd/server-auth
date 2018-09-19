55043afe89dcaf558307a6ddefa1a27d60380b03
Houzéfa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2018-09-19 11:26:14 +0200

    auth_saml: Backport base settings
    This essentially reverts changes regarding base settings done in rev
    b4329a9f5e53c9de6ed8f03c61018a0f17071fc2.

--- auth_saml/models/base_settings.py
+++ auth_saml/models/base_settings.py
@@ -7,16 +7,16 @@ from odoo import api, fields, models
 _SAML_UID_AND_PASS_SETTING = 'auth_saml.allow_saml.uid_and_internal_password'
 
 
-class ResConfigSettings(models.TransientModel):
-    """Inherit from res.config.settings to add a setting. This is only here
+class BaseConfigSettings(models.TransientModel):
+    """Inherit from base.config.settings to add a setting. This is only here
     for easier access; the setting is not actually stored by this (transient)
     collection. Instead, it is kept in sync with the
     "auth_saml.allow_saml.uid_and_internal_password" global setting. See
-    comments in the definition of the "res.config.settings" collection for
+    comments in the definition of the "base.config.settings" collection for
     details.
     """
 
-    _inherit = 'res.config.settings'
+    _inherit = 'base.config.settings'
 
     allow_saml_uid_and_internal_password = fields.Boolean(
         string='Allow SAML users to posess an Odoo password '
@@ -40,20 +40,42 @@ class ResConfigSettings(models.TransientModel):
 
         return (True if config_objs.value == '1' else False)
 
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        get_param = self.env['ir.config_parameter'].sudo().get_param
-        res.update(allow_saml_uid_and_internal_password=get_param(
-            'auth_saml.allow_saml_uid_and_internal_password'))
-        return res
+    @api.multi
+    def get_default_allow_saml_uid_and_internal_password(self, fields):
+        """Read the allow_saml_uid_and_internal_password setting. This function
+        is called when the form is shown.
+        """
+
+        ret = {}
+
+        if 'allow_saml_uid_and_internal_password' in fields:
+            ret['allow_saml_uid_and_internal_password'] = (
+                self.allow_saml_and_password()
+            )
+
+        return ret
 
     @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        set_param = self.env['ir.config_parameter'].sudo().set_param
-        if self.allow_saml_uid_and_internal_password:
-            self.allow_saml_uid_and_internal_password = \
-                self.allow_saml_and_password()
-        set_param('auth_saml.allow_saml_uid_and_internal_password',
-                  repr(self.allow_saml_uid_and_internal_password))
+    def set_allow_saml_uid_and_internal_password(self):
+        """Update the allow_saml_uid_and_internal_password setting. This
+        function is called when saving the form.
+        """
+
+        setting_value = (
+            '1' if self.allow_saml_uid_and_internal_password else '0'
+        )
+
+        config_obj = self.env['ir.config_parameter']
+        config_ids = config_obj.search(
+            [('key', '=', _SAML_UID_AND_PASS_SETTING)],
+            limit=1,
+        )
+
+        if config_ids:
+            config_ids.write({'value': setting_value})
+
+        else:
+            # The setting doesn't exist; create it.
+            config_obj.create(
+                {'key': _SAML_UID_AND_PASS_SETTING, 'value': setting_value},
+            )
